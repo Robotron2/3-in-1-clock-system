bd0952990f1ccffc120fd1ef4ea74b1ddef5c91b
Theophilus Adesola <theophilusadessola@gmail.com>
2022-09-19 12:44:50 +0100

    Added a beep feature that goes off when the countdown reaches zero

--- index.js
+++ index.js
@@ -16,3 +16,47 @@ const formatTime = (currentTime) => {
     return currentTime;
 };
 setInterval(clock, 1000);
+
+//Our Sound to be played once the countdown reaches zero.
+// let newSound = new Audio()
+//Countdown Section
+const countDown = () => {
+    //Get all the inputs.
+    let countedUserHour = userInputHr.value;
+    let countedUserMin = userInputMin.value;
+    let countedUserSec = userInputSec.value;
+    let beepForCountDown = new Audio("audio/Countdown.wav");
+    //Reduce the userInput for seconds by 1
+    countedUserSec--;
+    //Set the reduced value back to the user
+    userInputSec.value = countedUserSec;
+    //Reduce the userInput for seconds by 1 everysecond.
+    setTimeout(countDown, 1000);
+    //First case is when there is no user input for hour and input boxes and the seconds value is < 0
+    if (countedUserSec < 0 && countedUserMin == 0 && countedUserHour == 0) {
+        clearTimeout(countDown);
+        userInputHr.value = "00";
+        userInputMin.value = "00";
+        userInputSec.value = "00";
+        beepForCountDown.play();
+        setTimeout(stopCountdownBeep, 3000);
+    }
+    //Second case where the seconds is < 0 but the minute box is greater than zero. Seconds becomes 59.
+    if (countedUserSec < 0 && userInputMin.value > 0) {
+        userInputSec.value = 59;
+    }
+    // And we reduce the the minute also.
+    if (countedUserSec < 0 && countedUserMin > 0) {
+        userInputMin.value--;
+    }
+    // When the hour box is greater than zero, we want both minute and seconds boxes to return to 59
+    if (countedUserSec < 0 && userInputMin.value >= 0 && countedUserHour > 0) {
+        userInputSec.value = 59;
+        userInputMin.value = 59;
+        userInputHr.value--;
+    }
+};
+
+const stopCountdownBeep = () => {
+    beepForCountDown.pause();
+};
